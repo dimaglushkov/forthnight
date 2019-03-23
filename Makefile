b94ad9835547b962f11761985a9ffe0a6a618fa5
allacee <ddimaglushkov@ya.ru>
2019-03-23 23:26:52 +0300

    part2 files unpacked

---
+++ Makefile
@@ -0,0 +1,31 @@
+# ------------------------------------------------
+# Forthress, a Forth dialect 
+#
+# Author: igorjirkov@gmail.com
+# Date  : 02-04-2018
+#
+# ------------------------------------------------
+
+ASM         = nasm
+ASMFLAGS    = -felf64 -g -Isrc/ 
+LINKER      = ld
+
+all: bin/forthress
+	
+bin/forthress: obj/forthress.o obj/util.o
+	mkdir -p bin 
+	$(LINKER) -o bin/forthress  $(LINKERFLAGS) -o bin/forthress obj/forthress.o obj/util.o $(LIBS)
+
+obj/forthress.o: src/forthress.asm src/macro.inc src/words.inc src/util.inc
+	mkdir -p obj
+	$(ASM) $(ASMFLAGS) src/forthress.asm -o obj/forthress.o
+
+obj/util.o: src/util.inc src/util.asm
+	mkdir -p obj
+	$(ASM) $(ASMFLAGS) src/util.asm -o obj/util.o
+
+clean: 
+	rm -rf bin obj
+
+.PHONY: clean
+
---
+++ src/forthress.asm
@@ -0,0 +1,54 @@
+; ------------------------------------------------
+; Forthress 2, a Forth dialect 
+;
+; Author: igorjirkov@gmail.com
+; Date  : 08-03-2019
+;
+; This is the main Forthress 2 file which defines the entry point
+; Please define words inside "words.inc"
+; ------------------------------------------------
+
+global _start
+%include "util.inc"
+%include "macro.inc"
+
+%define pc r15
+%define w r14
+%define rstack r13
+
+section .text
+
+%include "kernel.inc"   ;  Minimal word set is here
+%include "words.inc"    ;  Predefined words are here
+
+section .bss
+
+; return stack end-----;
+resq 1023              ;
+rstack_start: resq 1   ;
+; return stack start---;
+
+user_mem: resq 65536   ; global data for user
+
+section .data
+
+last_word: dq _lw      ; stores a pointer to the last word in dictionary
+                       ; should be placed after all words are defined
+dp: dq user_mem        ; current global data pointer
+stack_start:  dq 0     ; stores a saved address of data stack
+ 
+section .text
+
+_start: 
+    mov rstack, rstack_start
+    mov [stack_start], rsp
+
+    ;;  push to stack to set up initial stack state
+    mov pc, program
+
+next:                  
+    mov w, pc
+    add pc, 8
+    mov w, [w]
+    jmp [w]
+
